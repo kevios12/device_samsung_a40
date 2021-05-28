501fccb3f53f314e5d2ea9ffda36561dc4bf0ca0
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-05-29 01:23:35 +0200

    Update lineage_a40.mk

--- lineage_a40.mk
+++ lineage_a40.mk
@@ -23,14 +23,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from a40 device
 $(call inherit-product, device/samsung/a40/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a40
 PRODUCT_NAME := lineage_a40
+PRODUCT_DEVICE := a40
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := Samsung Galaxy A40
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_RELEASE_NAME := samsung Samsung Galaxy A40
