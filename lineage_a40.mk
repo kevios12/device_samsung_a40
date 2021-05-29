b3c5648fcda0b826134e6f16ca17206206268cc2
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-05-29 02:07:37 +0200

    Update lineage_a40.mk

--- lineage_a40.mk
+++ lineage_a40.mk
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2020 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,13 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from a40 device
 $(call inherit-product, device/samsung/a40/device.mk)
@@ -31,3 +28,11 @@ PRODUCT_NAME := lineage_a40
 PRODUCT_DEVICE := a40
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-A405FN
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+TARGET_VENDOR := samsung
+TARGET_VENDOR_PRODUCT_NAME := a40xx
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+# BUILD_FINGERPRINT := samsung/a40xx/a40:10/QP1A.190711.020/A405FNXXU3BTK4:user/release-keys
