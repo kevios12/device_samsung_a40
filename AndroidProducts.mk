3a78fbf963154d213710bd1d71406e0f12fb7b95
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-05-29 14:43:20 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,4 +18,6 @@ PRODUCT_MAKEFILES := \
      $(LOCAL_DIR)/lineage_a40.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a40-userdebug
+     lineage_a40-userdebug \
+     lineage_a40-eng \
+     lineage_a40-user
